e19de66dfe64565ea0bdc3f07f32c0704a6a32ce
camillo860 <camillo860@hotmail.com>
2024-04-17 16:18:31 +0200

    Esempio Albergo Java

---
+++ albergoEsempio/src/exception/FileException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author raffaele
+ */
+public class FileException extends Exception {
+    public FileException(String arg){
+        
+    }
+}
